3205e130b6a470bf97d460ab395b06ff9544fdcf
Niv <nivdan7@gmial.com>
2022-04-28 08:27:55 +0300

    payment service AT req #1.3 also added template for re #1.2 to next version impl issue #41

--- Market/tests/Acceptance/Tests/SystemTests.java
+++ Market/tests/Acceptance/Tests/SystemTests.java
@@ -48,4 +48,67 @@ public class SystemTests extends MarketTests{
         assertTrue(res.errorOccurred());
     }
 
+    /**
+     * Requirement: Add/Edit/Change ExternalService - #1.2
+     * //TODO: req #1.2 (next version)
+     */
+    @Test
+    @DisplayName("req: #1.2.1 - success test")
+    void Add_Connection_With_ExternalService_Success() {
+        //TODO: next version
+    }
+
+    @Test
+    @DisplayName("req: #1.2.1 - fail test [illegal service]")
+    void Add_Connection_With_ExternalService_Fail1() {
+        //TODO: next version
+    }
+
+    /**
+     * Requirement: payment with external service - #1.3
+     * Assumptions: credit card number is 16 digits long,
+     * card num : 1111222233334444 - valid    cvv: 111 - valid
+     */
+    @Test
+    @DisplayName("req: #1.3 - success test")
+    void PaymentService_Success() {
+        CreditCard creditCard = new CreditCard("1111222233334444", "1122", "111");
+
+        ATResponseObj<String> response = market.pay(creditCard, 10);
+
+        assertFalse(response.errorOccurred());
+        assertNotNull(response.value);
+        assertNotEquals(response.value, "");
+
+    }
+
+    @Test
+    @DisplayName("req: #1.3 - fail test [invalid payment details]")
+    void PaymentService_Fail1() {
+        CreditCard creditCard = new CreditCard("1111222233334444", "1110", "111"); //invalid exp date
+
+        ATResponseObj<String> response = market.pay(creditCard, 10);
+
+        assertTrue(response.errorOccurred());
+    }
+
+    @Test
+    @DisplayName("req: #1.3 - fail test [invalid price]")
+    void PaymentService_Fail2() {
+        CreditCard creditCard = new CreditCard("1111222233334444", "1123", "111"); //invalid exp date
+
+        ATResponseObj<String> response = market.pay(creditCard, -1);
+        assertTrue(response.errorOccurred());
+        response = market.pay(creditCard, 0);
+        assertTrue(response.errorOccurred());
+    }
+
+    @Test
+    @DisplayName("req: #1.3 - fail test [invalid input]")
+    void PaymentService_Fail3() {
+        ATResponseObj<String> response = market.pay(null, 10);
+        assertTrue(response.errorOccurred());
+    }
+
+
 }
