af2a85f17e910839c5cbf99354e2ac07011f8878
lielk <62788163+lielk@users.noreply.github.com>
2022-05-07 21:22:39 +0300

    #fronted-login and register page
    run 'npm start' and go to http://localhost:3000/
    to see the pages

---
+++ fronted/my-app/src/App.js
@@ -0,0 +1,61 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Login from './components/login'
+import SignUp from './components/signUp'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import MainPage from "./components/mainPage";
+import InitMarket from "./components/initMarket";
+
+function App() {
+  return (
+      <Router>
+        <div className="App">
+          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+              <Link className="navbar-brand" to={'/sign-in'}>
+              </Link>
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/sign-in'}>
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/sign-up'}>
+                      Sign up
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/get-in'}>
+                      Visit as a guest
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/init-market'}>
+                      Init market
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Routes>
+                <Route exact path="/" element={<Login />} />
+                <Route path="/sign-in" element={<Login />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/get-in" element={<MainPage />} />
+                <Route path="/init-market" element={<InitMarket />} />
+
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </Router>
+  )
+}
+
+export default App;
---
+++ fronted/my-app/src/components/login.jsx
@@ -0,0 +1,106 @@
+import React, {Component, useState} from "react";
+import MainPage from "./mainPage";
+import {Route, Routes, Switch} from "react-router-dom";
+import ReactDOM from "react-dom";
+import SignUp from "./signUp";
+import InitMarket from "./initMarket";
+
+function Login(props) {
+let ans =true;
+    function errorMsgChangeHandler(event) {
+        setErrorMsgState(event);
+    }
+
+    function FLogin(connID, name, pass) {
+        return new Response();
+    }
+
+    const [enteredPassword, setPasswordState] = useState("");
+    const [enteredEmail, setEmailState] = useState("");
+
+    const [enteredErrorMsg, setErrorMsgState] = useState("");
+
+    async function submitHandler(event) {
+
+        event.preventDefault();
+
+        //apiHttp.
+        //connid= call guest visit
+        const loginResponse = FLogin(
+            enteredEmail,
+            enteredPassword,
+        );
+
+        //boolean ans = loginResponse.value
+        let ans = true;
+        let errorMsg = "invalid blablala";
+        if (!ans)
+            errorMsgChangeHandler(errorMsg);
+        else {
+            errorMsgChangeHandler("good");
+
+        }
+
+    }
+
+    return (
+        <form method="post" className="contact-form" >
+            <h3>Sign In</h3>
+            <div className="mb-3">
+                <label>1email address {enteredEmail}</label>
+                <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter email"
+                    // onChange={emailChangeHandler}
+
+                />
+            </div>
+            <div className="mb-3">
+                <label>Password</label>
+                <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                    // onChange={nameChangeHandler}
+
+                />
+            </div>
+            <div className="mb-3">
+                <div className="custom-control custom-checkbox">
+                    <input
+                        type="checkbox"
+                        className="custom-control-input"
+                        id="customCheck1"
+                    />
+                    <label className="custom-control-label" htmlFor="customCheck1">
+                        Remember me
+                    </label>
+                </div>
+            </div>
+
+            <div className="d-grid">
+                <button type="submit" onClick={submitHandler} className="btn btn-primary">
+                    Submit
+                </button>
+            </div>
+
+            <h1 style={{color: "red"}}>{enteredErrorMsg}</h1>
+
+            <p className="forgot-password text-right">
+                Forgot <a href="#">password?</a>
+            </p>
+            {ans?(
+                <MainPage
+                />
+                ):("")
+            }
+        </form>
+
+
+
+    )
+
+}
+
+export default Login;
