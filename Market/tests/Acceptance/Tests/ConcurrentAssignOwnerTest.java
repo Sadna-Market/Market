1902dafa62ce31fa094489470b1c13de06fbb28f
Niv <nivdan7@gmial.com>
2022-05-08 23:58:49 +0300

    added concurrent AT issue #143

---
+++ Market/tests/Acceptance/Tests/ConcurrentAssignOwnerTest.java
@@ -0,0 +1,60 @@
+package Acceptance.Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import Acceptance.Obj.*;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+
+public class ConcurrentAssignOwnerTest extends MarketTests {
+    String uuid;
+    Boolean owner1Success = null;
+    Boolean owner2Success = null;
+
+    @BeforeEach
+    public void setUp() {
+        initMarketWithSysManagerAndItems();
+        registerMemberData();
+        populateItemsAndStore();
+        uuid = market.guestVisit();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        market = null; //for garbage collector
+    }
+
+    @Test
+    @DisplayName("assign manager at the same time")
+    public void assignSameTime() throws InterruptedException {
+        User anotherOwner = generateUser();
+        User toBeOwner = generateUser();
+        assertTrue(market.register(uuid, anotherOwner.username, anotherOwner.password));
+        assertTrue(market.register(uuid, toBeOwner.username, toBeOwner.password));
+
+        //login as member
+        ATResponseObj<String> memberID = market.login(uuid, member);
+        assertFalse(memberID.errorOccurred());
+        uuid = memberID.value;
+        assertTrue(market.assignNewOwner(uuid, existing_storeID, anotherOwner));
+        uuid = market.logout(uuid).value;
+
+        //action
+        Thread owner1 = new Thread(() -> {
+            ATResponseObj<String> id = market.login(market.guestVisit(), member);
+            owner1Success = market.assignNewOwner(id.value, existing_storeID, toBeOwner);
+        });
+        Thread owner2 = new Thread(() -> {
+            ATResponseObj<String> id = market.login(market.guestVisit(), anotherOwner);
+            owner2Success = market.assignNewManager(id.value, existing_storeID, toBeOwner);
+        });
+        owner2.start();
+        owner1.start();
+        owner2.join();
+        owner1.join();
+        assertTrue(owner1Success ^ owner2Success);
+
+
+    }
+}
