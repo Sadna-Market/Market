72af848a2477a3b98b921f5d873d2e8a9831a4b0
dorelad-1994 <dor0@post.bgu.ac.il>
2022-05-10 21:26:24 +0300

    fixed market tests

--- Market/tests/main/System/Server/Domain/Market/MarketTest.java
+++ Market/tests/main/System/Server/Domain/Market/MarketTest.java
@@ -228,7 +228,7 @@ class MarketTest {
     @ParameterizedTest
     @ValueSource(ints = {1,2,0})
     void searchProductByCategory(int i) {
-        assertEquals(i==0? 4:3,market.searchProductByCategory(i).value.size());
+        assertEquals(i>0? 5:0,market.searchProductByCategory(i).value.size());
     }
     @DisplayName("searchProductByCategory  -  failure")
     @ParameterizedTest
