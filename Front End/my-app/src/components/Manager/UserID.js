f5d28e3a7d3958e99ad45746072fe893dfe6ce7f
yakovelmaleh <87435007+yakovelmaleh@users.noreply.github.com>
2022-05-25 22:08:07 +0300

    #166 add Users

---
+++ Front End/my-app/src/components/Manager/UserID.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+//import "./StoreID.css";
+import Card from "../UI/Card";
+
+const UserID = (props) => {
+  //todo: remove user
+  const clickHandler = () => {
+    props.onEnterToStore(props.id);
+  };
+  return (
+    <li>
+      <Card className="store-item">
+        <div className="store-item__description">
+          <h2>{props.email}</h2>
+          <h2>{props.state}</h2>
+          <button onClick={clickHandler}>Remove User</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default UserID;
