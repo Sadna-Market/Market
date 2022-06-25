7860540c43cf5ce89afb97bd493f970cf631935e
Niv <nivdan7@gmial.com>
2022-06-25 19:28:25 +0300

    env properites

--- market-spring-boot/src/main/java/com/example/demo/configuration/JsonInit.java
+++ market-spring-boot/src/main/java/com/example/demo/configuration/JsonInit.java
@@ -3,19 +3,19 @@ package com.example.demo.configuration;
 public class JsonInit {
     public JsonUser admin;
     public boolean initState;
-    public String  statePath;
+    public String statePath;
     public String url;
 
     public String spring_datasource_url;
-    public String      spring_datasource_username;
-    public String       spring_datasource_password;
-    public String      spring_jpa_hibernate_ddl_auto;
-    public String      spring_jpa_show_sql;
-    public String       spring_jpa_properties_hibernate_dialect;
-    public String       spring_jpa_properties_hibernate_format_sql;
+    public String spring_datasource_username;
+    public String spring_datasource_password;
+    public String spring_jpa_hibernate_ddl_auto;
+    public String spring_jpa_show_sql;
+    public String spring_jpa_properties_hibernate_dialect;
+    public String spring_jpa_properties_hibernate_format_sql;
+    public Integer server_port;
 
-    public JsonUser getAdmin()
-    {
+    public JsonUser getAdmin() {
         return admin;
     }
 }
